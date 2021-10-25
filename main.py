023cace07504ec0b90cbf9b1cafd49a663343d92
justddreamm <lagovsp@gmail.com>
2021-10-25 21:25:21 +0300

    fully reworked

--- main.py
+++ main.py
@@ -1,87 +1,119 @@
+from prettytable import PrettyTable
 import matplotlib.pyplot as plt
 import random
 import numpy as np
 
 
 class Being:
-	def __init__(self):
-		self.x = 0
-		self.y = 0
+	__max_del_per: None
+	__borders: None
+	__fun: None
 
-	def rand(self, xl, xr, yl, yr):
-		self.x = random.uniform(xl, xr)
-		self.y = random.uniform(yl, yr)
+	@staticmethod
+	def set_max_delta_percentage(del_per):
+		Being.__max_del_per = del_per
+
+	@staticmethod
+	def set_borders(coordinates):
+		Being.__borders = coordinates
+
+	@staticmethod
+	def custom_sort_key(being):
+		return being.fit()
+
+	@staticmethod
+	def set_function(f):
+		Being.__fun = f
+
+	@staticmethod
+	def show_meta():
+		with open('results.txt', 'w') as file:
+			file.write('Beings settings so far\n')
+			file.write(f'Maximum mutation deviation percent: {Being.__max_del_per}\n')
+			file.write(f'Borders of the field are: x from {Being.__borders[0][0]} to {Being.__borders[0][1]}, '
+			           f'y from {Being.__borders[1][0]} to {Being.__borders[1][1]}\n')
+
+	def __init__(self):
+		self.__chromosomes = [0, 0]
+
+	def rand(self):
+		self.__chromosomes = [
+			random.uniform(Being.__borders[0][0], Being.__borders[0][1]),
+			random.uniform(Being.__borders[1][0], Being.__borders[1][1])]
 		return self
 
 	def set(self, x, y):
-		self.x = x
-		self.y = y
+		self.__chromosomes = [x, y]
 		return self
 
-	def rand_x(self, xl, xr):
-		self.x = random.uniform(xl, xr)
+	def set_x(self, x):
+		self.__chromosomes[0] = x
 		return self
 
-	def rand_y(self, yl, yr):
-		self.y = random.uniform(yl, yr)
+	def set_y(self, y):
+		self.__chromosomes[1] = y
 		return self
 
-	def fit(self, f):
-		return f(self.x, self.y)
+	def get_x(self):
+		return self.__chromosomes[0]
+
+	def get_y(self):
+		return self.__chromosomes[1]
+
+	def mutate(self, ind):
+		delta = Being.__max_del_per * (
+				Being.__borders[ind][1] - Being.__borders[ind][0])
+		val = random.random() * delta
+		if random.random() < 0.5:
+			val *= -1
+		if self.__chromosomes[ind] + val < Being.__borders[ind][0]:
+			self.__chromosomes[ind] = Being.__borders[ind][0]
+			return self
+		if self.__chromosomes[ind] + val > Being.__borders[ind][1]:
+			self.__chromosomes[ind] = Being.__borders[ind][1]
+			return self
+		self.__chromosomes[ind] += val
+		return self
+
+	def fit(self):
+		return Being.__fun(self.__chromosomes[0], self.__chromosomes[1])
+
+	def crossing(self, b):
+		return [
+			Being().set(self.__chromosomes[0], b.__chromosomes[1]),
+			Being().set(b.__chromosomes[0], self.__chromosomes[1])
+		]
 
 
-class Iteration:
-	def __init__(self, N, bs, f):
-		self.N = N
-		self.bs = bs
-		self.f = f
+class Generation:
+	def __init__(self, num, bs):
+		bs.sort(key = Being.custom_sort_key, reverse = True)
+		self.__bs = bs
+		self.__num = num
 
-	def __str__(self):
-		s = f'{self.N}\t'
+	def get_beings(self):
+		return self.__bs
 
-		if len(self.bs) > 0:
-			s += f'\t\t{self.bs[0].x}'
-			s += f'\t\t\t{self.bs[0].y}'
-			s += f'\t\t\t{self.bs[0].fit(self.f)}'
-			val = max_avg_fit(self.bs, self.f)
-			s += f'\t\t\t{val[0]}\t\t\t{val[1]}\n'
+	def max_avg_fit(self):
+		s = 0
+		for b in self.__bs:
+			s += b.fit()
+		return [self.__bs[0].fit(), s / len(self.__bs)]
 
-			for it in range(1, len(self.bs)):
-				s += f'\t\t\t{self.bs[it].x}'
-				s += f'\t\t\t{self.bs[it].y}'
-				s += f'\t\t\t{self.bs[it].fit(self.f)}\n'
-		return s
-
-	def get_best_xyz_list(self):
-		self.bs.sort(key = custom_sort_key, reverse = True)
-		best = self.bs[0]
-		return [best.x, best.y, best.fit(self.f)]
+	def get_best(self):
+		best = self.__bs[0]
+		return [best.get_x(), best.get_y(), best.fit()]
 
 
 def fun(xs, ys):
 	return np.exp(-np.power(xs, 2) - np.power(ys, 2))
-# return np.cos(xs) * np.cos(ys)
 
 
-def custom_sort_key(being):
-	return being.fit(fun)
-
-
-def generate_beings(num, xl, xr, yl, yr):
+def generate_beings(num):
 	bs = []
 	for it in range(num):
-		bs.append(Being().rand(xl, xr, yl, yr))
+		bs.append(Being().rand())
 	return bs
-
-
-def max_avg_fit(bs, f):
-	s = 0
-	best = 0
-	for b in bs:
-		s += b.fit(f)
-		if b.fit(f) > best:
-			best = b.fit(f)
-	return [best, s / len(bs)]
 
 
 def graph(f, xl, xr, yl, yr):
@@ -107,88 +139,91 @@ def fit_graph(xy_lists):
 	plt.show()
 
 
-def genetic(f, xl, xr, yl, yr, num, gens):
-	graph(f, xl, xr, yl, yr)
-
-	beings = generate_beings(num, xl, xr, yl, yr)
-	beings.sort(key = custom_sort_key, reverse = True)
-	iterations = [Iteration(0, beings, f)]
+def genetic(num, gens, probability):
+	beings = generate_beings(num)
+	gen = Generation(0, beings)
+	generations = [gen]
 	x_values = [0]
-	ym_values = [max_avg_fit(beings, f)[0]]
-	y_values = [max_avg_fit(beings, f)[1]]
+	ym_values = [gen.max_avg_fit()[0]]
+	y_values = [gen.max_avg_fit()[1]]
 
 	for N in range(1, gens + 1):
-		beings = [
-			Being().set(beings[0].x, beings[1].y),
-			Being().set(beings[1].x, beings[0].y),
-			Being().set(beings[0].x, beings[2].y),
-			Being().set(beings[2].x, beings[0].y),
-		]
-
-		# series of experiments showed that we should take into consideration
-		# whether the first coordinate mutated whilst judging if another one
-		# should be mutated as well. if one is mutated already, then the second
-		# one has its chance for this reduced
-
-		probability = 0.25
-		del_per = 5
-		x_delta = del_per / 100 * (xr - xl)
-		y_delta = del_per / 100 * (yr - yl)
+		progeny = beings[0].crossing(beings[1])
+		progeny.extend(beings[0].crossing(beings[2]))
+		beings = progeny
 
 		for b in beings:
 			if random.random() < probability:
-				val = random.uniform(0, 1) * x_delta
-				if random.random() < 0.5:
-					val *= -1
-				b.set(b.x + val, b.y)
-
-				if b.x < xl:
-					b.x = xl
-				if b.x > xr:
-					b.x = xr
-
+				b.mutate(0)
 			if random.random() < probability:
-				val = random.uniform(0, 1) * y_delta
-				if random.random() < 0.5:
-					val *= -1
-				b.set(b.x, b.y + val)
+				b.mutate(1)
 
-				if b.y < yl:
-					b.y = yl
-				if b.y > yr:
-					b.y = yr
+		gen = Generation(N, beings)
+		generations.append(gen)
 
-		beings.sort(key = custom_sort_key, reverse = True)
-		iterations.append(Iteration(N, beings, f))
+		x_values.append(N)
+		ym_values.append(gen.max_avg_fit()[0])
+		y_values.append(gen.max_avg_fit()[1])
 
-		if N < 10 or N % 10 == 0:
-			x_values.append(N)
-			ym_values.append(max_avg_fit(beings, f)[0])
-			y_values.append(max_avg_fit(beings, f)[1])
-
-	return [iterations, [x_values, y_values, ym_values]]
+	return [generations, [x_values, y_values, ym_values]]
 
 
-def show_result(its):
-	print('N\t\t\t\tX\t\t\t\t\tY\t\t\t\t\t\t\tFIT\t\t\t\t\t\tMAX '
-	      'FIT\t\t\t\t\tAVG FIT\n')
-	for num, it in enumerate(its):
-		if num < 10 or num % 10 == 0:
-			print(it)
-	b = its[len(its) - 1].get_best_xyz_list()
-	print(f'The best option so far is: x = {b[0]} y = {b[1]}, fit = {b[2]}\n')
+def show_result(gens):
+	ns, xs, ys, fits, max_fits, avg_fits = [], [], [], [], [], []
+	skip = ['', '', '', '']
+	for gn, gen in enumerate(gens):
+		maf = gen.max_avg_fit()
+		ns.append(gn)
+		max_fits.append(maf[0])
+		avg_fits.append(maf[1])
+		ns.extend(skip)
+		max_fits.extend(skip)
+		avg_fits.extend(skip)
+
+		for b in gen.get_beings():
+			xs.append(b.get_x())
+			ys.append(b.get_y())
+			fits.append(b.fit())
+		xs.append('')
+		ys.append('')
+		fits.append('')
+
+	table = PrettyTable()
+	titles = ['N', 'X', 'Y', 'FIT', 'MAX FIT', 'AVG FIT']
+	table.add_column(titles[0], ns)
+	table.add_column(titles[1], xs)
+	table.add_column(titles[2], ys)
+	table.add_column(titles[3], fits)
+	table.add_column(titles[4], max_fits)
+	table.add_column(titles[5], avg_fits)
+
+	b = gens[len(gens) - 1].get_best()
+	with open('results.txt', 'a') as file:
+		file.write(table.get_string())
+		file.write(f'\nThe best option so far is: x = {b[0]} y = {b[1]}, fit = {b[2]}\n')
 
 
 if __name__ == '__main__':
 	# setting the initial variables
-	x1 = -2
-	x2 = 2
-	y1 = -2
-	y2 = 2
-	n = 4
-	gen_m = 10
+	x1, y1 = -2, -2
+	x2, y2 = 2, 2
+	percentage = 5 / 100
+	borders = [[x1, x2], [y1, y2]]
 
-	# the very function
-	result = genetic(fun, x1, x2, y1, y2, n, gen_m)
+	beings_num = 4
+	gens_max = 100
+	mut_prob = 0.25
+
+	# applying the values
+	Being.set_max_delta_percentage(percentage)
+	Being.set_borders(borders)
+	Being.set_function(fun)
+
+	# genetic algorithm
+	result = genetic(beings_num, gens_max, mut_prob)
+
+	# printing the data
+	Being.show_meta()
+	graph(fun, x1, x2, y1, y2)
 	show_result(result[0])
 	fit_graph(result[1])
